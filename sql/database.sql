92ea31f050542c69b57162ad70483026dd0dc30f
dkanyugo <darrelkanyugo@gmail.com>
2024-10-05 22:40:05 +0300

    Added the database dump

--- sql/database.sql
+++ sql/database.sql
@@ -1,2 +1,63 @@
-DROPDATABASE IFEXISTS"darrel_157769";
-CREATEDATABASE IFNOTEXISTS"darrel_157769";
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.5.2-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: darrel_157769
+-- ------------------------------------------------------
+-- Server version	11.5.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `date_name` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `date_number` varchar(20) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES
+(1,'brian','abigael','brian@brooke.com','0712345678','0798765432',NULL),
+(2,'Darrel','Tracy','darrelkanyugo@gmail.com','0712345678','0783746258','I tried calling her after our first date and she hasn\'t responded in 2 weeks.'),
+(3,'Darrel','Tracy','darrelkanyugo@gmail.com','0712345678','0783746258','I tried calling her after our first date and she hasn\'t responded in 2 weeks.'),
+(4,'Darrel','Tracy','darrelkanyugo@gmail.com','0712345678','0783746258','I tried calling her after our first date and she hasn\'t responded in 2 weeks.'),
+(5,'Darrel','Donna','darrelkanyugo@gmail.com','0733657999','0712345678','She said that Harvey had called her to the office during our dinner at 11.00 pm.She has ghosted me since'),
+(6,'Darrel','Daenarys','darrelkanyugo@gmail.com','0733657226','0712343455','I guess she had to feed her dragons but why hasn\'t she responded to my letters. It has been 6 months already');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2024-10-05 22:39:26
