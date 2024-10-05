cb4f115b5fd5fee2edf792f1ad1eb61d9f38faf3
dkanyugo <darrelkanyugo@gmail.com>
2024-10-05 20:55:03 +0300

    Created verification file

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 require "load.php";
 
-if(isset($_POST["name"])){
-  $DbConn->add_request($_POST["name"], $_POST["dates"], $_POST[ "email" ], $_POST["p_no"], $_POST["dates_no"], $_POST["desc"]);
-  header("Location: ");
-  exit;
-}
-
 $ObjLayouts->head("Mars form");
-$ObjForms->sign_up_form($_SERVER["PHP_SELF"]);
+$ObjForms->sign_up_form("verify_email.php");
 $ObjLayouts->foot();
 ?>
---
+++ verify_email.php
@@ -0,0 +1,54 @@
+<?php
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+require 'load.php';
+
+//Create an instance; passing `true` enables exceptions
+//
+$mail = new PHPMailer(true);
+$verify = rand(10000, 99999);
+if (isset($_POST['verify'])){
+  if ($_POST['verify_user'] == $_POST['verify']){
+  $DbConn->add_request($_POST["name"], $_POST["dates"], $_POST[ "email" ], $_POST["p_no"], $_POST["dates_no"], $_POST["desc"]);
+  header("Location: ");
+  exit;
+
+  }
+}
+try {
+  //Server settings
+  $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+  $mail->isSMTP();                                            //Send using SMTP
+  $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+  $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+  $mail->Username   = 'darrel.kariuki@strathmore.edu';                     //SMTP username
+  $mail->Password   = 'vjcs loko qjsp xzgm';                               //SMTP password
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+  $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+  //Recipients
+  $mail->setFrom('darrel.kariuki@strathmore.edu', 'Mailer');
+  $mail->addAddress($_POST['email'], $_POST['name']);     //Add a recipient
+
+  //Content
+  $mail->isHTML(true);                                  //Set email format to HTML
+  $mail->Subject = "Verify your email for Second date update";
+  $mail->Body    = "Here is your verification code: <b>$verify</b>";
+  $mail->AltBody    = "Here is your verification code: $verify";
+
+  $mail->send();
+
+$ObjLayouts->head("Mars form");
+$ObjForms->verify_form($_POST, $verify);
+$ObjLayouts->foot();
+  
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+} 
+
+?>
